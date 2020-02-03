e3bd499df534efa465288d8264ae084a0c62ec35
voracious-module-9516 <as.nazeer2019@gmail.com>
2020-02-03 18:47:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+board = [ "   ","   ","   ","   ","   ","   ","   ","   ","   " ]
+
+
+def display_board(the_board)
+  puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
+  puts "-----------"
+  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
+  puts "-----------"
+  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
+end
+
+display_board(board)
